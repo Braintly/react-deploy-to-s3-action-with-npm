35c6e78d7c7249cb73e75e37c48ec9c7c7bbce76
dguevara-braintly <88737527+dguevara-braintly@users.noreply.github.com>
2022-09-16 19:34:15 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,15 @@ if [ -z "$AWS_S3_BUCKET" ]; then
   exit 1
 fi
 
-if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-  exit 1
-fi
+#if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+#  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+#  exit 1
+#fi
 
-if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-  exit 1
-fi
+#if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+#  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+#  exit 1
+#fi
 
 # Default to us-east-1 if AWS_REGION not set.
 if [ -z "$AWS_REGION" ]; then
@@ -32,12 +32,12 @@ NODE_ENV_PREPEND="NODE_ENV=${NODE_ENV:-production}"
 
 # Create a dedicated profile for this action to avoid conflicts
 # with past/future actions.
-aws configure --profile react-deploy-to-s3-action <<-EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_REGION}
-text
-EOF
+#aws configure --profile react-deploy-to-s3-action <<-EOF > /dev/null 2>&1
+#${AWS_ACCESS_KEY_ID}
+#${AWS_SECRET_ACCESS_KEY}
+#${AWS_REGION}
+#text
+#EOF
 
 # - Install dependencies
 # - Build react bundle
